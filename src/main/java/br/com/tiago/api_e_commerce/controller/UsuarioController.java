15ca405e08b935ec75a3842fcf71a463d8224f62
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2024-08-08 00:31:11 -0300

    criado novos métodos(crud) na classe UsuarioController

--- src/main/java/br/com/tiago/api_e_commerce/controller/UsuarioController.java
+++ src/main/java/br/com/tiago/api_e_commerce/controller/UsuarioController.java
@@ -1,14 +1,22 @@
 package br.com.tiago.api_e_commerce.controller;
 
-import br.com.tiago.api_e_commerce.dto.UsuarioDTO;
-import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tiago.api_e_commerce.dto.UsuarioDTO;
+import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/usuario")
@@ -20,13 +28,24 @@ public class UsuarioController {
     public void insereUsuarioaLista(){
         UsuarioDTO usuarioDTO1 = new UsuarioDTO();
         usuarioDTO1.setNome("Aline Vieira");
-        usuarioDTO1.setCpf("123");
+        usuarioDTO1.setCpf("1");
         usuarioDTO1.setEndereco("Rua matriz 23");
         usuarioDTO1.setEmail("aline@aline");
         usuarioDTO1.setTelefone("2222222");
         usuarioDTO1.setDataCadastro(LocalDateTime.now());
 
+        
+        UsuarioDTO usuarioDTO2 = new UsuarioDTO();
+        usuarioDTO2.setNome("Antônio Silva");
+        usuarioDTO2.setCpf("2");
+        usuarioDTO2.setEndereco("Rua cardoso de moraes 3");
+        usuarioDTO2.setEmail("antonio@antonio");
+        usuarioDTO2.setTelefone("33333333");
+        usuarioDTO2.setDataCadastro(LocalDateTime.now());
+
+
         usuario.add(usuarioDTO1);
+        usuario.add(usuarioDTO2);
     }
 
     @GetMapping()
@@ -34,5 +53,26 @@ public class UsuarioController {
         return usuario;
     }
 
+    @GetMapping("/{cpf}")
+    public UsuarioDTO filtraUsuario(@PathVariable String cpf){
+        return usuario.stream()
+        .filter(usuarioDto -> usuarioDto.getCpf()
+        .equals(cpf))
+        .findFirst()
+        .orElseThrow(() -> new RuntimeException("CPF NÃO ENCOTRADO"));
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsuarioDTO insereUsuario(@RequestBody @Validated UsuarioDTO usuarioDTO){
+        usuarioDTO.setDataCadastro(LocalDateTime.now());
+        usuario.add(usuarioDTO);
+        return  usuarioDTO;
+    }
+
+    @DeleteMapping("/{cpf}")
+    public boolean removeUsuario(@PathVariable String cpf){
+        return usuario.removeIf(usuarioDto -> usuarioDto.getCpf().equals(cpf));
+    }
 
 }
--- src/main/java/br/com/tiago/api_e_commerce/dto/UsuarioDTO.java
+++ src/main/java/br/com/tiago/api_e_commerce/dto/UsuarioDTO.java
@@ -1,20 +1,26 @@
 package br.com.tiago.api_e_commerce.dto;
 
 
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UsuarioDTO {
+    
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "Cpf é obrigatŕio")
     private String cpf;
+    
     private String endereco;
     private String email;
     private String telefone;
